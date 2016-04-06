dbb79d93267b52b58673991102802b69ef1ff97c
Samuel Turner <siberias.samt@gmail.com>
2016-04-07 01:05:27 +1000

    CryoStar: Define constructors and assignment operators for Vector3

--- Source/Types/Vector3.cpp
+++ Source/Types/Vector3.cpp
@@ -2,16 +2,49 @@
 
 #include "Vector3.h"
 
+#include <algorithm>
+
 using namespace CryoStar;
 
 Vector3::Vector3()
+	: X(0.0f)
+	, Y(0.0f)
+	, Z(0.0f)
 {
 
+}
+
+CryoStar::Vector3::Vector3(const Vector3& other)
+	: X(other.X)
+	, Y(other.Y)
+	, Z(other.Z)
+{
+
+}
+
+CryoStar::Vector3::Vector3(Vector3&& other)
+	: Vector3()
+{
+	Swap(*this, other);
 }
 
 Vector3::~Vector3()
 {
 
+}
+
+void CryoStar::Vector3::Swap(Vector3& first, Vector3& second)
+{
+	std::swap(first.X, second.X);
+	std::swap(first.Y, second.Y);
+	std::swap(first.Z, second.Z);
+}
+
+Vector3& CryoStar::Vector3::operator=(Vector3 other)
+{
+	Swap(*this, other);
+	
+	return *this;
 }
 
 Vector3::Vector3(float x, float y, float z)
@@ -20,4 +53,11 @@ Vector3::Vector3(float x, float y, float z)
 	, Z(z)
 {
 
+}
+
+CryoStar::Vector3::Vector3(float all)
+	: X(all)
+	, Y(all)
+	, Z(all)
+{
 }
--- Source/Types/Vector3.h
+++ Source/Types/Vector3.h
@@ -5,10 +5,17 @@ namespace CryoStar
 	class Vector3
 	{
 	public:
+		//Construction, destruction and assignment
 		Vector3();
+		Vector3(const Vector3& other);
+		Vector3(Vector3&& other);
 		~Vector3();
+		Vector3& operator=(Vector3 other);
+		void Swap(Vector3& first, Vector3& second);
 
+		//Custom constructors
 		Vector3(float x, float y, float z);
+		Vector3(float all);
 
 	public:
 		float X;
